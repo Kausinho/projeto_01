45ef7d70b88cf0e09dca8e67200fb13cc2665e37
Kausinho <kausinhobrunow@gmail.com>
2020-10-19 14:56:27 -0300

    Create MySql.php

---
+++ classes/MySql.php
@@ -0,0 +1,15 @@
+<?php
+	class MySql{
+
+		private static $pdo;
+
+		public static function conectar(){
+			if(isset(self::$pdo)){
+				return self::$pdo;
+			}else{
+				self::$pdo = new PDO('mysql:host='.HOST.';dbname='.DATABASE,USER,PASSWORD);
+			}
+		}
+
+	}
+?>
