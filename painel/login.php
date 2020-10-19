1e9cb753a837ae203c25b61e8d92b7ce04dcc82e
Kausinho <kausinhobrunow@gmail.com>
2020-10-19 11:38:43 -0300

    Update login.php

--- painel/login.php
+++ painel/login.php
@@ -4,12 +4,18 @@
 	<title>Painel de controle</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 	<link href="<?php echo INCLUDE_PATH_PAINEL?>css/style.css" rel="stylesheet">
 </head>
 <body>
 
 	<div class="box-login">
-		
+		<h2>Efetue o login:</h2>
+		<form>
+			<input type="text" name="user" placeholder="Login..." required>
+			<input type="password" name="password" placeholder="Senha..." required>
+			<input type="submit" name="acao" value="Logar!">
+		</form>
 	</div><!--box-login-->
 
 </body>
