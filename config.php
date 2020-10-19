2ee15048172453449cbd7388f5d37adf7013e641
Kausinho <kausinhobrunow@gmail.com>
2020-10-19 15:02:16 -0300

    Update config.php

--- config.php
+++ config.php
@@ -13,4 +13,9 @@
 
 	define('INCLUDE_PATH','http://localhost/projeto_01/');
 	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+	//Conectar com banco de dados!
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASSWORD','');
+	define('DATABASE','projeto_01');
 ?>
