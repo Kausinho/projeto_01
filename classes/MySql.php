079409c3d6baad5e76e4c14c440d7fa93a63241c
Kausinho <kausinhobrunow@gmail.com>
2020-10-19 14:59:48 -0300

    Update MySql.php

--- classes/MySql.php
+++ classes/MySql.php
@@ -4,11 +4,12 @@
 		private static $pdo;
 
 		public static function conectar(){
-			if(isset(self::$pdo)){
-				return self::$pdo;
-			}else{
-				self::$pdo = new PDO('mysql:host='.HOST.';dbname='.DATABASE,USER,PASSWORD);
-			}
+			try{
+				self::$pdo = new PDO('mysql:host='.HOST.';dbname='.DATABASE,USER,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+				self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+				}catch(Exception $e){
+					echo '<h2>Erro ao conectar</h2>';
+				}
 		}
 
 	}
