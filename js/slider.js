c67dcc1da0c8ab942faa2082a563db5c98d9fa30
Kausinho <kausinhobrunow@gmail.com>
2020-10-20 15:06:06 -0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -25,12 +25,12 @@ $(function(){
 
 	function changesSlide(){
 		setInterval(function(){
-			$('.banner-single').eq(curSlide).stop().fadeOut(2000);
+			$('.banner-single').eq(curSlide).animate({'opacity':'0'}2000);
 			curSlide++;
 			if(curSlide > maxSlide){
 				curSlide = 0;
 			}
-			$('.banner-single').eq(curSlide).stop().fadeIn(2000);
+			$('.banner-single').eq(curSlide).animate({'opacity':'1'}2000);
 			$('.bullets span').removeClass('active-slider');
 			$('.bullets span').eq(curSlide).addClass('active-slider');
 		},delay * 1000);
@@ -39,9 +39,9 @@ $(function(){
 
 	$('body').on('click','.bullets span',function(){
 		var currentBullet = $(this);
-		$('.banner-single').eq(curSlide).stop().fadeOut(1000);
+		$('.banner-single').eq(curSlide).animate({'opacity':'0'}2000);
 		curSlide = currentBullet.index();
-		$('.banner-single').eq(curSlide).stop().fadeIn(1000);
+		$('.banner-single').eq(curSlide).animate({'opacity':'1'}2000);
 		$('.bullets span').removeClass('active-slider');
 		currentBullet.addClass('active-slider');
 	});
