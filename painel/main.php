3fe34de43e0a065e0c01e38bddd1b59babb923d2
Kausinho <kausinhobrunow@gmail.com>
2020-10-20 18:53:33 -0300

    Update main.php

--- painel/main.php
+++ painel/main.php
@@ -1,2 +1,30 @@
+<?php
+	if(isset($_GET['loggout'])){
+		Painel::loggout();
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Painel de controle</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/font-awesome.min.css">
+	<link href="<?php echo INCLUDE_PATH_PAINEL?>css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="menu">
+	
+</div><!--menu-->
+<header>
+	<div class="center">
+		<div class="loggout">
+			<a href="<?php echo INCLUDE_PATH_PAINEL ?>?loggout"><i class="fa fa-window-close"></i></a>
+		</div><!--loggout-->
 
-<h2>Bem vindo ao painel <?php echo $_SESSION['user']; ?></h2>
+		<div class="clear"></div>
+	</div><!--center-->
+</header>
+</body>
+</html>
