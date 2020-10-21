1b5e3285138faf92534c476be00a46ceffe4bbff
Kausinho <kausinhobrunow@gmail.com>
2020-10-21 15:55:01 -0300

    Update main.php

--- painel/main.php
+++ painel/main.php
@@ -11,18 +11,26 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 	<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/font-awesome.min.css">
-	<link href="<?php echo INCLUDE_PATH_PAINEL?>css/style.css" rel="stylesheet">
+	<link href="<?php echo INCLUDE_PATH_PAINEL?>css/style.css" rel="stylesheet" />
 </head>
 <body>
 
 <div class="menu">
 	<div class="box-usuario">
+		<?php
+			if($_SESSION['img'] == ''){
+		?>
 		<div class="avatar-usuario">
 			<i class="fa fa-user"></i>
 		</div><!--avatar-usuario-->
+		<?php }else{ ?>
+			<div class="imagem-usuario">
+				<img src="<?php echo INCLUDE_PATH_PAINEL ?>uploads/<?php echo $_SESSION['img']; ?>" />
+			</div><!--avatar-usuario-->
+		<?php } ?>
 		<div class="nome-usuario">
 			<p><?php echo $_SESSION['nome']; ?></p>
-			<p><?php echo pegaCargo(); ?></p>
+			<p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
 		</div><!--nome-usuario-->
 	</div><!--box-usuario-->
 </div><!--menu-->
