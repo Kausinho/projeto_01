7ec2f84fd30e63091de77502afaa8d5a11e57930
Kausinho <kausinhobrunow@gmail.com>
2020-10-21 22:29:12 -0300

    Update main.php

--- painel/main.php
+++ painel/main.php
@@ -16,23 +16,40 @@
 <body>
 
 <div class="menu">
-	<div class="box-usuario">
-		<?php
-			if($_SESSION['img'] == ''){
-		?>
-		<div class="avatar-usuario">
-			<i class="fa fa-user"></i>
-		</div><!--avatar-usuario-->
-		<?php }else{ ?>
-			<div class="imagem-usuario">
-				<img src="<?php echo INCLUDE_PATH_PAINEL ?>uploads/<?php echo $_SESSION['img']; ?>" />
-			</div><!--avatar-usuario-->
-		<?php } ?>
-		<div class="nome-usuario">
-			<p><?php echo $_SESSION['nome']; ?></p>
-			<p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
-		</div><!--nome-usuario-->
-	</div><!--box-usuario-->
+	<div class="menu-wraper">
+			<div class="box-usuario">
+				<?php
+					if($_SESSION['img'] == ''){
+				?>
+					<div class="avatar-usuario">
+						<i class="fa fa-user"></i>
+					</div><!--avatar-usuario-->
+				<?php }else{ ?>
+					<div class="imagem-usuario">
+						<img src="<?php echo INCLUDE_PATH_PAINEL ?>uploads/<?php echo $_SESSION['img']; ?>" />
+					</div><!--avatar-usuario-->
+				<?php } ?>
+				<div class="nome-usuario">
+					<p><?php echo $_SESSION['nome']; ?></p>
+					<p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
+				</div><!--nome-usuario-->
+			</div><!--box-usuario-->
+		<div class="items-menu">
+			<h2>Cadastro</h2>
+			<a href="">Cadastrar Depoimentos</a>
+			<a href="">Cadastrar Serviços</a>
+			<a href="">Cadastrar Slides</a>
+			<h2>Gestão</h2>
+			<a href="">Listar Depoimentos</a>
+			<a href="">Listar Serviços</a>
+			<a href="">Listar Slides</a>
+			<h2>Administração do painel</h2>
+			<a href="">Editar Usuário</a>
+			<a href="">Adicionar Usuários</a>
+			<h2>Configuração Geral</h2>
+			<a href="">Editar</a>
+		</div><!--items-menu-->
+	</div><!--menu-wraper-->
 </div><!--menu-->
 
 <header>
@@ -47,5 +64,54 @@
 		<div class="clear"></div>
 	</div><!--center-->
 </header>
+
+<div class="content">	
+
+	<div class="box-content left w100">
+		<!--TROCAR POR CÓDIGO PHP-->
+		<h2><i class="fa fa-home"></i> Painel de Controle - <?php echo $nomeEmpresa ?></h2>
+
+		<div class="box-metricas">
+			<div class="box-metrica-single">
+				<div class="box-metrica-wraper">
+					<h2>Usuários Online</h2>
+					<p>10</p>
+				</div><!--box-metrica-wraper-->
+			</div><!--box-metrica-single-->
+			<div class="box-metrica-single">
+				<div class="box-metrica-wraper">
+					<h2>Total de Vistas</h2>
+					<p>100</p>
+				</div><!--box-metrica-wraper-->
+			</div><!--box-metrica-single-->
+			<div class="box-metrica-single">
+				<div class="box-metrica-wraper">
+					<h2>Vistas de Hoje</h2>
+					<p>3</p>
+				</div><!--box-metrica-wraper-->
+			</div><!--box-metrica-single-->
+		</div><!--box-metricas-->
+
+	</div><!--box-content-->
+
+	<!--
+	<div class="box-content left w100">
+
+	</div>
+
+	<div class="box-content left w50">
+
+	</div>
+
+	<div class="box-content rigth w50">
+
+	</div>
+	-->
+
+	<div class="clear"></div>	
+
+</div><!--content-->
+<script src="<?php echo INCLUDE_PATH ?>js/jquery.js"></script>
+<script src="<?php echo INCLUDE_PATH_PAINEL ?>js/main.js"></script>
 </body>
 </html>
