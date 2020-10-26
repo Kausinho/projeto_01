b2de625e58377a195bddfe3a095f57d8ded48872
Kausinho <kausinhobrunow@gmail.com>
2020-10-26 10:53:16 -0300

    Update Painel.php

--- classes/Painel.php
+++ classes/Painel.php
@@ -12,6 +12,20 @@
 			header('Location: '.INCLUDE_PATH.'painel/');
 		}
 
+		public static function carregarPagina(){
+			if(isset($_GET['url'])){
+				$url = explode('/',$_GET['url']);
+				if(file_exists('pages/'.$url[0].'.php')){
+					include('pages/'.$url[0].'.php');
+				}else{
+					//Página não existe.
+					header('Location: '.INCLUDE_PATH.'painel/');
+				}
+			}else{
+				include('pages/home.php');
+			}
+		}
+
 	}
 
 ?>
