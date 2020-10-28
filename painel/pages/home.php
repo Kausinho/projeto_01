46c12da5f7537b3b2915bc7a2dd41206cd0f6c67
Kausinho <kausinhobrunow@gmail.com>
2020-10-27 22:43:28 -0300

    Update home.php

--- painel/pages/home.php
+++ painel/pages/home.php
@@ -1,11 +1,14 @@
-<div class="box-content">
+<?php
+	$usuariosOnline = Painel::listarUsuariosOnline();
+?>
+<div class="box-content w100">
 	<h2><i class="fa fa-home"></i> Painel de Controle - <?php echo NOME_EMPRESA ?></h2>
 
 	<div class="box-metricas">
 		<div class="box-metrica-single">
 			<div class="box-metrica-wraper">
 				<h2>Usuários Online</h2>
-				<p>10</p>
+				<p><?php echo count($usuariosOnline); ?></p>
 			</div><!--box-metrica-wraper-->
 		</div><!--box-metrica-single-->
 		<div class="box-metrica-single">
@@ -36,14 +39,15 @@
 			</div><!--row-->
 
 			<?php
-				for($i = 0; $i < 1; $i++){
+				foreach ($usuariosOnline as $key => $value) {
+					
 			?>
 			<div class="row">
 				<div class="col">
-					<span>199.199.199.199</span>
+					<span><?php echo $value['ip']?></span>
 				</div><!--col-->
 				<div class="col">
-					<span>27/10/2020 10:00:00</span>
+					<span><?php echo date('d/m/Y H:i:s',strtotime($value['ultima_acao'])) ?></span>
 				</div><!--col-->
 				<div class="clear"></div>
 			</div><!--row-->
