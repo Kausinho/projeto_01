a12b02140edd4d5a6774d7ded09b23a84c93da48
Kausinho <kausinhobrunow@gmail.com>
2020-11-03 12:19:56 -0300

    Update main.php

--- painel/main.php
+++ painel/main.php
@@ -36,18 +36,18 @@
 			</div><!--box-usuario-->
 		<div class="items-menu">
 			<h2>Cadastro</h2>
-			<a href="<?php echo INCLUDE_PATH_PAINEL ?>cadastrar-depoimento">Cadastrar Depoimentos</a>
-			<a href="">Cadastrar Serviços</a>
-			<a href="">Cadastrar Slides</a>
+			<a <?php selecionadoMenu('cadastrar-depoimento'); ?> href="<?php echo INCLUDE_PATH_PAINEL ?>cadastrar-depoimento">Cadastrar Depoimentos</a>
+			<a <?php selecionadoMenu('cadastrar-servico'); ?> href="">Cadastrar Serviços</a>
+			<a <?php selecionadoMenu('cadastrar-slide'); ?> href="">Cadastrar Slides</a>
 			<h2>Gestão</h2>
-			<a href="">Listar Depoimentos</a>
-			<a href="">Listar Serviços</a>
-			<a href="">Listar Slides</a>
+			<a <?php selecionadoMenu('listar-depoimentos'); ?> href="">Listar Depoimentos</a>
+			<a <?php selecionadoMenu('listar-servicos'); ?> href="">Listar Serviços</a>
+			<a <?php selecionadoMenu('listar-slides'); ?> href="">Listar Slides</a>
 			<h2>Administração do painel</h2>
-			<a href="<?php echo INCLUDE_PATH_PAINEL?>editar-usuario">Editar Usuário</a>
-			<a href="">Adicionar Usuários</a>
+			<a <?php selecionadoMenu('editar-usuario'); ?> href="<?php echo INCLUDE_PATH_PAINEL ?>editar-usuario">Editar Usuário</a>
+			<a <?php selecionadoMenu('adicionar-usuario'); ?> <?php verificaPermissaoMenu(2); ?> href="<?php echo INCLUDE_PATH_PAINEL ?>adicionar-usuario">Adicionar Usuários</a>
 			<h2>Configuração Geral</h2>
-			<a href="">Editar</a>
+			<a <?php selecionadoMenu('editar-site'); ?> href="">Editar Site</a>
 		</div><!--items-menu-->
 	</div><!--menu-wraper-->
 </div><!--menu-->
