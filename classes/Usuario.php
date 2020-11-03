82ad72e717ecd055f2f0479bb0e92f8bbce40c9c
Kausinho <kausinhobrunow@gmail.com>
2020-11-03 17:48:04 -0300

    Update Usuario.php

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -12,9 +12,19 @@
 		}
 
 		public static function userExists($user){
+			$sql = Mysql::conectar()->prepare("SELECT `id` FROM `tb_admin.usuarios` WHERE user=?");
+			$sql->execute(array($user));
+			if($sql->rowcount() == 1){
+				return true;
+			}else{
+				return false;
+			}
+		}
 
+		public static function cadastrarUsuario($user,$senha,$imagem,$nome,$cargo){
+			$sql = Mysql::conectar()->prepare("INSERT INTO `tb_admin.usuarios` VALUES (null,?,?,?,?,?)");
+			$sql->execute(array($user,$senha,$imagem,$nome,$cargo));
 		}
 
 	}
-
 ?>
