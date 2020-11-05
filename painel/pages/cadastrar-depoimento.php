cafcf0efaf670aa801b5d8514b3d9356a1d12337
Kausinho <kausinhobrunow@gmail.com>
2020-11-05 20:25:41 -0300

    Update cadastrar-depoimento.php

--- painel/pages/cadastrar-depoimento.php
+++ painel/pages/cadastrar-depoimento.php
@@ -5,74 +5,33 @@
 
 		<?php
 			if(isset($_POST['acao'])){
-				$login = $_POST['login'];
-				$nome = $_POST['nome'];
-				$senha = $_POST['password'];
-				$imagem = $_FILES['imagem'];
-				$cargo = $_POST['cargo'];
-
-				if($login == ''){
-					Painel::alert('erro',' O login está vazio!');
-				}else if($nome == ''){
-					Painel::alert('erro',' O nome está vazio!');
-				}else if($senha == ''){
-					Painel::alert('erro',' A senha está vazia!');
-				}else if($cargo == ''){
-					Painel::alert('erro',' O cargo precisa estar selecionado!');
-				}else if($imagem['name'] == ''){
-					Painel::alert('erro','A imagem precisa estar selecionada!');
-				}else{
-					//Podemos cadastrar!
-					if($cargo >= $_SESSION['cargo']){
-						Painel::alert('erro','Você precisa selecionar um cargo menor que o seu!');
-					}else if(Painel::imagemValida($imagem) == false){
-						Painel::alert('erro','O formato especificado não está correto!');
-					}else if(Usuario::userExists($login)){
-						Painel::alert('erro','O login já existe, selecione outro por favor!');
-					}else{
-						//Apenas cadastrar no banco de dados!
-						$usuario = new Usuario();
-						$imagem = Painel::uploadFile($imagem);
-						$usuario->cadastrarUsuario($login,$senha,$imagem,$nome,$cargo);
-						Painel::alert('sucesso','O cadastro do usuário '.$login.' foi feito com sucesso!');
-					}
-				}
 				
+				if(Painel::insert($_POST)){
+					Painel::alert('sucesso','O cadastro do depoimento foi realizado com sucesso!');	
+				}else{
+					Painel::alert('erro','Campos vázios não são permitidos!');
+				}
 				
 			}	
 		?>
 
 		<div class="form-group">
-			<label>Login:</label>
-			<input type="text" name="login">
+			<label>Nome da pessoa:</label>
+			<input type="text" name="nome">
 		</div><!--form-group-->
 		
 		<div class="form-group">
-			<label>Nome:</label>
-			<input type="text" name="nome">
-		</div><!--form-group-->
-		<div class="form-group">
-			<label>Senha:</label>
-			<input type="password" name="password">
+			<label>Depoimento:</label>
+			<textarea name="depoimento"></textarea>
 		</div><!--form-group-->
 
 		<div class="form-group">
-			<label>Cargo:</label>
-			<select name="cargo">
-				<?php
-					foreach (Painel::$cargos as $key => $value) {
-						if($key < $_SESSION['cargo']) echo '<option value="'.$key.'">'.$value.'</option>';
-					}
-				?>
-			</select>
+			<label>Data:</label>
+			<input type="text" name="data">
 		</div><!--form-group-->
 
 		<div class="form-group">
-			<label>Imagem</label>
-			<input type="file" name="imagem"/>
-		</div><!--form-group-->
-
-		<div class="form-group">
+			<input type="hidden" name="nome_tabela" value="tb_site.depoimentos" />
 			<input type="submit" name="acao" value="Cadastrar!">
 		</div><!--form-group-->
 
