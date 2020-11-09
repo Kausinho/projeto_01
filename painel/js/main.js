6a03808d0c851aa4bb12616d501371298f58a65f
Kausinho <kausinhobrunow@gmail.com>
2020-11-09 19:11:11 -0300

    Update main.js

--- painel/js/main.js
+++ painel/js/main.js
@@ -1,9 +1,9 @@
 $(function(){
 
-	var open = true;
+	var open  = true;
 	var windowSize = $(window)[0].innerWidth;
 
-	var targetSizeMenu = (windowSize <= 400) ? 200 : 300;
+	var targetSizeMenu = (windowSize <= 400) ? 200 : 250;
 
 	if(windowSize <= 768){
 		$('.menu').css('width','0').css('padding','0');
@@ -12,7 +12,7 @@ $(function(){
 
 	$('.menu-btn').click(function(){
 		if(open){
-			//O menu está aberto, precisamos fechar e adaptar o conteúdo geral do painel
+			//O menu está aberto, precisamos fechar e adaptar nosso conteudo geral do painel
 			$('.menu').animate({'width':0,'padding':0},function(){
 				open = false;
 			});
@@ -23,11 +23,12 @@ $(function(){
 		}else{
 			//O menu está fechado
 			$('.menu').css('display','block');
-			$('.menu').animate({'width':targetSizeMenu+'px','padding':'10px'},function(){
+			$('.menu').animate({'width':targetSizeMenu+'px','padding':'10px 0'},function(){
 				open = true;
 			});
-			$('.content,header').css('width','calc(100% - 300px)');
-			$('.content,header').animate({'left':targetSizeMenu+'px'},function(){
+			if(windowSize > 768)
+				$('.content,header').css('width','calc(100% - 250px)');
+				$('.content,header').animate({'left':targetSizeMenu+'px'},function(){
 				open = true;
 			});
 		}
@@ -35,16 +36,35 @@ $(function(){
 
 	$(window).resize(function(){
 		windowSize = $(window)[0].innerWidth;
+		targetSizeMenu = (windowSize <= 400) ? 200 : 250;
 		if(windowSize <= 768){
 			$('.menu').css('width','0').css('padding','0');
-			$('.content','header').css('width','100%').css('left','0');
+			$('.content,header').css('width','100%').css('left','0');
 			open = false;
 		}else{
+			$('.menu').animate({'width':targetSizeMenu+'px','padding':'10px 0'},function(){
+				open = true;
+			});
+
+			$('.content,header').css('width','calc(100% - 250px)');
+			$('.content,header').animate({'left':targetSizeMenu+'px'},function(){
 			open = true;
-			$('.content,header').css('width','calc(100% - 250px)').css('left','250px');
-			$('.menu').css('width','250px').css('padding','10px 0');
+			});
 		}
+
 	})
-	
+
+	$('[formato=data]').mask('99/99/9999');
+
+	$('[actionBtn=delete]').click(function(){
+			var txt;
+			var r = confirm("Deseja excluir o registro?");
+			if (r == true) {
+			    return true;
+			} else {
+			    return false;
+			}
+	})
+
 
 })
