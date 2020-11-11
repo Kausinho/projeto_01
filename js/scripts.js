4155b3a13d83cc41c67043e302e40cfd465d386b
Kausinho <kausinhobrunow@gmail.com>
2020-11-11 14:32:17 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,18 +13,18 @@ $(function(){
 		*/
 
 		//Abrir ou fechar sem efeitos
-		
 		/*
+		
 		if(listaMenu.is(':hidden') == true){
-			listaMenu.show();
+			//listaMenu.show();
 			listaMenu.css('display','block');
 		}
 		else{
-			listaMenu.hide();
+			//listaMenu.hide();
 			listaMenu.css('display','none');
 		}
 		*/
-		
+
 		if(listaMenu.is(':hidden') == true){
 			//fa fa-times
 			//fa fa-bars
@@ -40,13 +40,11 @@ $(function(){
 			icone.addClass('fa-bars');
 			listaMenu.slideToggle();
 		}
-		
 
 	});
 
-
 	if($('target').length > 0){
-		//O elemento existe, portanto temos que dar um scroll em algum elemento
+		//O elemento existe, portanto precisamos dar o scroll em algum elemento.
 		var elemento = '#'+$('target').attr('target');
 
 		var divScroll = $(elemento).offset().top;
@@ -54,18 +52,23 @@ $(function(){
 		$('html,body').animate({scrollTop:divScroll},2000);
 	}
 
+
+
 	carregarDinamico();
 	function carregarDinamico(){
 		$('[realtime]').click(function(){
 			var pagina = $(this).attr('realtime');
 			$('.container-principal').hide();
-			$('.container-principal').load(include_path+'/pages/'+pagina+'.php');
-
-		setTimeout(function(){
+			$('.container-principal').load(include_path+'pages/'+pagina+'.php');
 			
-		},1000);
+			setTimeout(function(){
+				initialize();
+				addMarker(-27.609959,-48.576585,'',"Minha casa",undefined,false);
+
+			},1000);
 
 			$('.container-principal').fadeIn(1000);
+			window.history.pushState('', '',pagina);
 
 			return false;
 		})
