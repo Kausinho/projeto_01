6eff8c08cb51c1089d58a7eb4da399ad3a44ef55
Kausinho <kausinhobrunow@gmail.com>
2020-11-16 15:07:14 -0300

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,7 @@
 	<section class="banner-container">
-		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/img1.png');" class="banner-single"></div><!--banner-single-->
-		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/img2.png');" class="banner-single"></div><!--banner-single-->
-		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/img3.png');" class="banner-single"></div><!--banner-single-->
+		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/img1.jpg');" class="banner-single"></div><!--banner-single-->
+		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/img2.jpg');" class="banner-single"></div><!--banner-single-->
+		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/img3.jpg');" class="banner-single"></div><!--banner-single-->
 			<div class="overlay"></div><!--overlay-->
 			<div class="center">
 			<form method="post">
@@ -23,7 +23,7 @@
 		</div><!--w50-->
 		<div class="w50 left">
 			<!--Pegar imagem depois-->
-			<img class="rigth" src="<?php echo INCLUDE_PATH; ?>images/bg1.png">
+			<img class="rigth" src="<?php echo INCLUDE_PATH; ?>images/bg1.jpg">
 		</div><!--w50-->
 		<div class="clear"></div>
 		</div><!--center-->
