ec405bed5cf2a658411ac039d21abeb3a95717a1
Kausinho <kausinhobrunow@gmail.com>
2020-11-25 18:09:05 -0300

    Create noticias.php

---
+++ pages/noticias.php
@@ -0,0 +1,43 @@
+<section class="header-noticias">
+	<div class="center">
+		<h2><i class="fa fa-bullhorn" aria-hidden="true"></i></h2>
+		<h2>Acompanhe as úlitmas <b>notícias do portal</b></h2>
+	</div><!--center-->	
+</section>
+
+<section class="container-portal">
+	<div class="center">
+		<div class="sidebar">
+			<div class="box-content-sidebar">
+				<h3><i class="fa fa-search" aria-hidden="true"></i> Realizar uma busca:</h3>
+				<form>
+					<input type="text" name="busca" placeholder="O que deseja procurar?" required>
+					<input type="submit" name="acao" value="Pesquisar!">
+				</form>
+			</div><!--box-content-sidebar-->
+			<div class="box-content-sidebar">
+				<h3><i class="fa fa-list-alt" aria-hidden="true"></i> Selecione a categoria:</h3>
+				<form>
+					<select name="categoria">
+						<option value="esportes">Esportes</option>
+						<option value="esportes">Geral</option>
+					</select>
+				</form>
+			</div><!--box-content-sidebar-->
+			<div class="box-content-sidebar">
+				<h3><i class="fa fa-user" aria-hidden="true"></i> Sobre o autor:</h3>
+					<div class="autor-box-portal">
+						<div class="box-img-autor"></div>
+						<div class="texto-autor-portal text-center">
+							<h3>Klaussio Brunow Carvalho</h3>
+							<p>Proin sed purus sed nibh bibendum sollicitudin eget in ipsum. Quisque ex purus, sollicitudin vitae imperdiet sed, luctus facilisis ligula. Quisque leo nunc, vulputate non metus eget, tristique porta odio. Quisque nec vehicula justo. Donec dignissim mauris vitae hendrerit cursus. Ut gravida iaculis erat at auctor. Aenean at erat consectetur dui elementum luctus.</p>
+						</div><!--texto--autor-portal-->
+					</div><!--autor-box-portal-->	
+			</div><!--box-content-sidebar-->	
+		</div><!--sidebar-->
+
+
+		<div class="clear"></div>
+	</div><!--center-->	
+
+</section><!--container--portal-->
