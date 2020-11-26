5127d949af42782defc8fcf3176e8a68990a0123
Kausinho <kausinhobrunow@gmail.com>
2020-11-26 18:32:06 -0300

    Create noticia_single.php

---
+++ pages/noticia_single.php
@@ -0,0 +1,17 @@
+<section class="noticia-single">
+	<div class="center">
+		<article>
+			<h1>Título da minha notícia</h1>
+			<h3>Título em h3</h3>
+			<h2>Título em h2</h2>
+			<p>Quisque a nunc metus. Integer ullamcorper malesuada eros, vitae euismod nisl lacinia et. Morbi consequat tristique risus in hendrerit. Sed ultrices est vitae elementum suscipit. Donec lacinia enim enim, in tristique dolor auctor vel. Etiam egestas vel urna et laoreet. Nullam quis tincidunt nisl. Integer posuere justo fermentum dui aliquam iaculis</p>
+			<p>Quisque a nunc metus. Integer ullamcorper malesuada eros, vitae euismod nisl lacinia et. Morbi consequat tristique risus in hendrerit. Sed ultrices est vitae elementum suscipit. Donec lacinia enim enim, in tristique dolor auctor vel. Etiam egestas vel urna et laoreet. Nullam quis tincidunt nisl. Integer posuere justo fermentum dui aliquam iaculis</p>
+			<ul>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+			</ul>
+			<img src="<?php echo INCLUDE_PATH ?>bg-form5.png" />
+		</article>
+	</div><!--center-->
+</section>
